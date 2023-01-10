d1c393a8cc882c8155d830460a044a0a36da797f
Karol <kariurek@gmail.com>
2023-01-10 12:43:11 +0100

    Feat: add framer motion scroll bound reveal

--- src/components/LandingPage/Section/Section.tsx
+++ src/components/LandingPage/Section/Section.tsx
@@ -1,20 +1,36 @@
 import clsx from 'clsx'
-import { motion } from 'framer-motion'
+import { motion, useSpring } from 'framer-motion'
 import { Props as wordsForSection, SpacedText } from '../SpacedText/SpacedText'
 import classes from './Section.module.scss'
+
+import { useScroll } from 'framer-motion'
+import { useRef } from 'react'
 
 type Props = { words: wordsForSection[] }
 
 export const Section = ({ words }: Props) => {
   const wordContainerCss = clsx(classes.wordsContainer, 'words-container')
 
+  const ref = useRef(null)
+  const { scrollYProgress, scrollY } = useScroll({
+    target: ref,
+    offset: ['end end', '70% 70%'],
+  })
+
+  const springProgress = useSpring(scrollYProgress, {
+    stiffness: 400,
+    damping: 60,
+  })
+
   return (
     <div className={classes.root}>
       <motion.div
-        initial={{ opacity: 0, scale: 0.8 }}
-        whileInView={{ opacity: 1, scale: 1 }}
-        transition={{ type: 'spring', stiffness: 100, duration: 10 }}
-        viewport={{ once: false, margin: '-200px' }}
+        ref={ref}
+        // initial={{ opacity: 0, scale: 0.8 }}
+        // whileInView={{ opacity: 1, scale: 1 }}
+        // transition={{ type: 'spring', stiffness: 100, duration: 10 }}
+        // viewport={{ once: false, margin: '-200px' }}
+        style={{ opacity: springProgress, scale: springProgress }}
         className={wordContainerCss}
       >
         {words.map((word, index) => (
